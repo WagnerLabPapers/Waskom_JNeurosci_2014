303c01a23a77792309948a42474c6c5e6d5b1424
Michael Waskom <mwaskom@stanford.edu>
2013-12-02 15:29:27 -0800

    Adding project.py file for lyman

---
+++ project.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for DKsort built on Tue Sep  3 17:31:41 2013
+
+# Default experiment name
+default_exp = 'dksort'
+
+# Data directory is where the input data lives
+data_dir = '../data'
+
+# Analysis directory is where anything written by these scripts will live
+analysis_dir = '../analysis'
+
+# Working directory is where data lives during workflow execution
+working_dir = '../cache'
+
+# Set this to True to remove the working directory after each excecution
+rm_working_dir = False
+
